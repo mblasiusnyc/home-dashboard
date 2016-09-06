80a599f2ab2325faa3309e8c6b92be4266793d44
Jeff Stephens <jefftheman45@gmail.com>
2016-09-06 04:16:58 +0000

    Push network devices to Firebase every minute

--- presence-scanner.js
+++ presence-scanner.js
@@ -6,25 +6,56 @@
  */
 
 var exec = require('child_process').exec;
-var cmd = 'sudo arp-scan -l';
 
-var macRegex = /(\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})/;
-var ipRegex = /(?:[0-9]{1,3}\.){3}[0-9]{1,3}/;
+var Firebase = require('firebase');
+firebaseApp = null;
 
-exec(cmd, function(error, stdout, stderr) {
-  var outputLines = stdout.split('\n');
+function initFirebase() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyDEHc7ws4S0JeV2HuDvMMjTFqbO-TDkgd8",
+    authDomain: "home-dashboard-9604a.firebaseapp.com",
+    databaseURL: "https://home-dashboard-9604a.firebaseio.com",
+    storageBucket: "home-dashboard-9604a.appspot.com",
+  };
+  firebaseApp = Firebase.initializeApp(firebaseConfig);
+}
 
-  for (var i = outputLines.length - 1; i >= 0; i--) {
-    var macMatch = outputLines[i].match(macRegex);
-    var ipMatch = outputLines[i].match(ipRegex);
+function saveToFirebase(key, value) {
+  firebaseApp.database().ref().child(key).push(value);
+}
 
-    if (macMatch === null || ! macMatch) {
-      continue;
+function scanNetwork() {
+  var scanDate = Date.now();
+  var dbKey = "presences"
+
+  console.log("Scanning network... ", scanDate);
+
+  var macRegex = /(\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2})/;
+  var ipRegex = /(?:[0-9]{1,3}\.){3}[0-9]{1,3}/;
+
+  var cmd = 'sudo arp-scan -l';
+  exec(cmd, function(error, stdout, stderr) {
+    var outputLines = stdout.split('\n');
+
+    for (var i = outputLines.length - 1; i >= 0; i--) {
+      var macMatch = outputLines[i].match(macRegex);
+      var ipMatch = outputLines[i].match(ipRegex);
+
+      if (macMatch === null || ! macMatch) {
+        continue;
+      }
+
+      var presenceData = {
+        macAddress: macMatch[0],
+        ipAddress: ipMatch[0],
+        timestamp: scanDate,
+      };
+
+      saveToFirebase(dbKey, presenceData);
     }
+  });
+}
 
-    var macAddress = macMatch[0];
-    var ipAddress = ipMatch[0];
+initFirebase();
 
-    console.log("Found " + macAddress + " - " + ipAddress);
-  }
-});
+setInterval(scanNetwork, 60000);
